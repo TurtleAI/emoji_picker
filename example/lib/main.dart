5f2502309d0d83ba68568fbb2a79e8cac3180ecd
Venkat Dinavahi <vendiddy@gmail.com>
2020-07-02 16:04:46 -0300

    Yes very close

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,22 +24,12 @@ class MainPage extends StatefulWidget {
   MainPageState createState() => new MainPageState();
 }
 
-final emojisForCategory = EmojiCollection.getForCategory(Category.SMILEYS);
-final smileyPages = chunk(emojisForCategory, 21);
+final emojiDataSource = DefaultEmojiDataSource();
 
 class MainPageState extends State<MainPage> {
-  final PageController pageController = PageController();
-
-  ValueNotifier<ScrollPosition> pageScrollPosition =
-      ValueNotifier(ScrollPosition());
-
   @override
   void initState() {
     super.initState();
-
-    pageController.addListener(() {
-      pageScrollPosition.value = scrollPositionForController(pageController);
-    });
   }
 
   @override
@@ -67,66 +57,13 @@ class MainPageState extends State<MainPage> {
           },
         ),
         SizedBox(height: 50),
-        SizedBox(
-          height: 200,
-          child: EmojiPageView(
-            pageController: pageController,
-            pages: smileyPages,
-            pageSize: 21,
-          ),
+        EmojiPickerSheet(
+          dataSource: emojiDataSource,
+          onEmojiPressed: (emoji) {
+            print("PRESSED $emoji");
+          },
         ),
-        HorizontalScrollPositionIndicator(position: pageScrollPosition),
       ],
-    );
-  }
-}
-
-class ScrollPosition {
-  final double offset;
-  final double width;
-  const ScrollPosition({this.offset = 0, this.width = 0});
-}
-
-ScrollPosition scrollPositionForController(PageController controller) {
-  final position = controller.position;
-  final offset = controller.offset;
-  final totalWidth = position.maxScrollExtent - position.minScrollExtent;
-  return ScrollPosition(
-    offset: offset / totalWidth,
-    width: position.viewportDimension / totalWidth,
-  );
-}
-
-class HorizontalScrollPositionIndicator extends StatelessWidget {
-  final ValueNotifier<ScrollPosition> position;
-
-  HorizontalScrollPositionIndicator({this.position});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 20,
-      color: Colors.yellow,
-      child: AnimatedBuilder(
-        animation: position,
-        builder: (context, _) {
-          final curPosition = this.position.value;
-          return Align(
-            alignment: FractionalOffset(curPosition.offset, 0),
-            child: FractionallySizedBox(
-              alignment: Alignment.centerLeft,
-              heightFactor: 1,
-              widthFactor: curPosition.width,
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.blue.withOpacity(0.3),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
